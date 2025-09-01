8c60cf041e425aea21d5ace75c71a10b9492a8e4
Leozera2k <leofsousa98@gmail.com>
2025-09-01 11:46:04 -0300

    preparação do projeto

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import gitLogo from '../../assets/gitLogo.png';
+import { Container } from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt="gitHub logo"/>
+    </Container>
   );
 }
 
